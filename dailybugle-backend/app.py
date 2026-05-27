072594d6ba653083ebbf1a2d44352ac1d0a10554
Kamalesh <kamaleshbabu622@gmail.com>
2026-05-27 14:22:24 +0530

    fix : connect frontend with backend

--- dailybugle-backend/app.py
+++ dailybugle-backend/app.py
@@ -27,7 +27,7 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["https://daily-bugle-mu.vercel.app/"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
